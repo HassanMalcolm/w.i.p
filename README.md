9d0b894da56726c32a5ba807979e00f5d4aaf7dd
HassanMalcolm <152523111+HassanMalcolm@users.noreply.github.com>
2025-01-28 12:44:51 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# w.i.p
+# lemonade-stand-sim
+An idle game with twist
---
+++ src/main.js
@@ -0,0 +1,32 @@
+const tickRate = 1000 / 30; // 30 FPS
+let clickStrength = 1;
+let score = 0;
+
+let juicer = new Building('Juicer', 0.1, 15);
+let sugar = new Building('Sugar', 1, 100);
+
+let swoleForearms1 = new SwoleArms('Swole Forearms I', 100, juicer);
+
+function scorePlusPlus() {
+    score += clickStrength;
+}
+
+function incScore() {
+    score += juicer.cps;
+    score += sugar.cps;
+}
+
+function updateButtons() {
+    juicer.buttonState();
+    sugar.buttonState();
+    swoleForearms1.buttonState();
+}
+
+function updatePage() {
+    incScore();
+    updateButtons();
+    document.getElementById('score').innerHTML =
+        '$' + Math.floor(score).toLocaleString();
+}
+
+setInterval(updatePage, tickRate);
